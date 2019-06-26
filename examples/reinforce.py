Alpha decay, fixed reward calculation

examples/reinforce.py
@@ -41,9 +41,9 @@ class ReinforceModel(tf.keras.Model):
         return tf.squeeze(tf.random.categorical(self(state, training=False), 1)).numpy()
 
 class ReinforceNetwork(RLModel):
-    def __init__(self, env, alpha, gamma=.99, init_epsilon = 1.0, min_epsilon = .01, batch_size = 32, batch_normalization = False, writer = None):
+    def __init__(self, env, alpha, alpha_decay, gamma=.99, init_epsilon = 1.0, min_epsilon = .01, batch_size = 32, batch_normalization = False, writer = None):
         assert isinstance(env.action_space, Discrete)
-        super().__init__(env, alpha, gamma, init_epsilon, min_epsilon)
+        super().__init__(env, alpha, alpha_decay, gamma, init_epsilon, min_epsilon)
         self.batch_size = batch_size
         self.model = ReinforceModel(action_space_size=env.action_space.n, batch_normalization=batch_normalization)
         self.baseline_model = BaselineModel()
@@ -53,6 +53,7 @@ class ReinforceNetwork(RLModel):
         self.step = 0
         self.writer = writer
         self.max_len_episode = 10000
+        self.reward_history = []
 
     def _calculate_cumulative_reward(self, batch_reward, gamma):
         batch_reward = deque(batch_reward)
@@ -67,19 +68,20 @@ class ReinforceNetwork(RLModel):
     def greedy_action(self, state):
         return self.model.greedy_action(tf.convert_to_tensor([state], tf.float32))
 
-    def training_episode_impl(self, episode_lenght):
+    def training_episode_impl(self, episode_lenght, debug=False):
         # Initialize the environment and get its initial state.
         self.state = self.env.reset()
 
         buffer = []
 
+        episode_reward = 0
         for t in range(self.max_len_episode):
             action = self.epsilon_greedy_action(self.state)
             next_state, reward, done, info = self.env.step(
                 action)  # Let the environment to execute the action, get the next state of the action, the reward of the action, whether the game is done and extra information.
-            reward = -10. if done else reward  # Give a large negative reward if the game is over.
             buffer.append((self.state, action, reward, next_state,
-                                  1 if done else 0))  # Put the (state, action, reward, next_state) quad back into the experience replay pool.
+                                              1 if done else 0))  # Put the (state, action, reward, next_state) quad back into the experience replay pool.
+            episode_reward+=reward
             self.state = next_state
 
             if done:  # Exit this round and enter the next episode if the game is over.
@@ -94,7 +96,7 @@ class ReinforceNetwork(RLModel):
         batch_cumulative_rewards = self._calculate_cumulative_reward(batch_reward, self.gamma)
 
         # cummulative reward for the whole episode
-        tf.summary.scalar("reward", batch_cumulative_rewards[0], step=self.step)
+        tf.summary.scalar("episode_reward", episode_reward, step=self.step)
 
         with tf.GradientTape() as tape:
             v = self.baseline_model(batch_state)
@@ -117,4 +119,10 @@ class ReinforceNetwork(RLModel):
             tf.summary.scalar("loss", loss, step=self.step)
         grads = tape.gradient(loss, self.model.variables)
         self.optimizer.apply_gradients(grads_and_vars=zip(grads, self.model.variables))
+
+        if debug:
+            self.reward_history.append(episode_reward)
+            avg_reward = np.mean(self.reward_history[-10:])
+            best_reward = np.max(self.reward_history)
+            print("[step:{}], best:{}, avg:{:.2f}, alpha:{:.4f}, epsilon:{}".format(self.step, best_reward, avg_reward, self.alpha, self.epsilon))
         self.step += 1

test/ReinforceHyperoptTest.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 import gym
 from hyperopt import fmin, tpe, hp
 import tensorflow as tf
+import math
 
 # Imports for the HParams plugin
 from tensorboard.plugins.hparams import api_pb2
@@ -18,18 +19,24 @@ class DeepQLearningHyperoptTest(TestCase):
     def setUp(self):
         super().setUp()
         self._alpha_list = []
+        self._alpha_decay_list = []
         self._gamma_list = []
         self._init_epsilon_list = []
+        self._n_exploration_episodes = []
         self._batch_norm_list= []
         self._log_dir = "/tmp/logdir/reinforce_cart_pole"
 
     def _create_experiment_summary(self):
         alpha_list_val = struct_pb2.ListValue()
         alpha_list_val.extend(self._alpha_list)
+        alpha_decay_list_val = struct_pb2.ListValue()
+        alpha_decay_list_val.extend(self._alpha_decay_list)
         gamma_list_val = struct_pb2.ListValue()
         gamma_list_val.extend(self._gamma_list)
         init_epsilon_list_val = struct_pb2.ListValue()
         init_epsilon_list_val.extend(self._init_epsilon_list)
+        n_exploration_episodes_val = struct_pb2.ListValue()
+        n_exploration_episodes_val.extend(self._n_exploration_episodes)
         batch_norm_list_val = struct_pb2.ListValue()
         batch_norm_list_val.extend(self._batch_norm_list)
         return hparams_summary.experiment_pb(
@@ -39,6 +46,10 @@ class DeepQLearningHyperoptTest(TestCase):
                                    display_name='Learning rate',
                                    type=api_pb2.DATA_TYPE_FLOAT64,
                                    domain_discrete=alpha_list_val),
+                api_pb2.HParamInfo(name='alpha_decay',
+                                   display_name='Learning rate decay',
+                                   type=api_pb2.DATA_TYPE_FLOAT64,
+                                   domain_discrete=alpha_decay_list_val),
                 api_pb2.HParamInfo(name='gamma',
                                    display_name='Reward discount factor',
                                    type=api_pb2.DATA_TYPE_FLOAT64,
@@ -47,6 +58,10 @@ class DeepQLearningHyperoptTest(TestCase):
                                    display_name='Initial exploration',
                                    type=api_pb2.DATA_TYPE_FLOAT64,
                                    domain_discrete=init_epsilon_list_val),
+                api_pb2.HParamInfo(name='n_exploration_episodes',
+                                   display_name='Initial exploration',
+                                   type=api_pb2.DATA_TYPE_FLOAT64,
+                                   domain_discrete=n_exploration_episodes_val),
                 api_pb2.HParamInfo(name='batch_norm',
                                    display_name='Batch normalization',
                                    type=api_pb2.DATA_TYPE_BOOL,
@@ -62,45 +77,48 @@ class DeepQLearningHyperoptTest(TestCase):
         )
 
     def _experiment(self, args):
-        alpha, gamma, init_epsilon, batch_norm = args
-        hparams = {'alpha':alpha, 'gamma':gamma, 'init_epsilon':init_epsilon, 'batch_norm':batch_norm}
+        alpha, alpha_decay, gamma, init_epsilon, n_exploration_episodes, batch_norm = args
+        hparams = {'alpha':alpha, 'alpha_decay':alpha_decay, 'gamma':gamma, 'init_epsilon':init_epsilon, 'n_exploration_episodes':n_exploration_episodes, 'batch_norm':batch_norm}
         self._alpha_list.append(alpha)
+        self._alpha_decay_list.append(alpha_decay)
         self._gamma_list.append(gamma)
         self._init_epsilon_list.append(init_epsilon)
+        self._n_exploration_episodes.append(n_exploration_episodes)
         self._batch_norm_list.append(batch_norm)
 
-        writer = tf.summary.create_file_writer(self._log_dir + "/alpha_{}_gamma_{:.3f}_init_eps{}_bn_{}".format(alpha, gamma, init_epsilon, batch_norm))
+        writer = tf.summary.create_file_writer(self._log_dir + "/alpha_{}_alpha_decay_{}_gamma_{:.3f}_init_eps{}_n_explor_{}_bn_{}"
+                                               .format(alpha, alpha_decay, gamma, init_epsilon, n_exploration_episodes, batch_norm))
         with writer.as_default():
             summary_start = hparams_summary.session_start_pb(hparams=hparams)
 
             env = CartPoleRewardWrapper(gym.make('CartPole-v1'))
-            reinforce = ReinforceNetwork(env, alpha=alpha, gamma=gamma, init_epsilon=init_epsilon, min_epsilon=0.0,
+            reinforce = ReinforceNetwork(env, alpha=alpha, alpha_decay=alpha_decay, gamma=gamma, init_epsilon=init_epsilon, min_epsilon=0.0,
                                          batch_normalization=batch_norm, writer=writer)
             num_episodes = 1000
-            episode_lenth = 1000
             for episode in range(num_episodes):
-                reinforce.training_episode(num_exploration_episodes=int(num_episodes * 2/3), episode_lenght=episode_lenth)
+                reinforce.training_episode(num_exploration_episodes=n_exploration_episodes, debug=True)
 
-            average_cmulative_reward = reinforce.evaluate_average_sum_reward(5)
-            print('Average sum reward after episode:{} for alpha: {}, gamma: {}, init_eps: {}, batch_norm: {}, reward: {}'.
-                  format(episode, alpha, gamma, init_epsilon, batch_norm, average_cmulative_reward))
+            average_sum_reward = reinforce.evaluate_average_sum_reward(5)
+            print('Average sum reward after episode:{} for alpha: {}, alpha_decay: {}, gamma: {}, init_eps: {}, n_exploration_episodes: {}, batch_norm: {}, reward: {}'.
+                  format(episode, alpha, alpha_decay, gamma, init_epsilon, n_exploration_episodes, batch_norm, average_sum_reward))
             summary_end = hparams_summary.session_end_pb(api_pb2.STATUS_SUCCESS)
-            tf.summary.scalar('sum_reward', average_cmulative_reward, step=1, description="Average sum reward")
+            tf.summary.scalar('sum_reward', average_sum_reward, step=1, description="Average sum reward")
             tf.summary.import_event(tf.compat.v1.Event(summary=summary_start).SerializeToString())
             tf.summary.import_event(tf.compat.v1.Event(summary=summary_end).SerializeToString())
 
         # hyperopt needs negative value to minimize provided function properly based on fmin (no fmax alternative yet..)
-        return -average_cmulative_reward
+        return -average_sum_reward
 
     def test_reinforce_cart_pole(self):
         log_dir = "/tmp/logdir/reinforce_cart_pole"
 
-        space = [hp.loguniform('alpha', 5e-4, 5e-3), hp.uniform('gamma', .99, .998), hp.uniform('init_epsilon', 0.0, 1.0),
-                 hp.choice('batch_norm', [True, False])]
+        space = [hp.loguniform('alpha', math.log(1e-4), math.log(1e-2)), hp.uniform('alpha_decay', .995, .999),
+                 hp.uniform('gamma', .99, .998), hp.uniform('init_epsilon', 0.0, 0.5),
+                 hp.uniform('n_exploration_episodes', 0, 500), hp.choice('batch_norm', [True, False])]
         # minimize the values
         best = fmin(self._experiment, space, algo=tpe.suggest, max_evals=50)
-        print("best hyperparameters: alpha={}, gamma={:.3f}, init_epsilon:{}, batch_norm:{}".format(best['alpha'],
-                                                            best['gamma'], best['init_epsilon'], best['batch_norm']))
+        print("best hyperparameters: alpha={}, alpha_decay={}, gamma={:.3f}, init_epsilon:{}, batch_norm:{}".format(best['alpha'],
+            best['alpha_decay'], best['gamma'], best['init_epsilon'], best['batch_norm']))
 
         # all hyper parameters are know after all experiments
         exp_summary = self._create_experiment_summary()

test/ReinforceTest.py
@@ -12,11 +12,10 @@ class ReinforceTests(TestCase):
     def test_cart_pole(self):
         env = CartPoleRewardWrapper(gym.make('CartPole-v1'))
         writer = tf.summary.create_file_writer("/tmp/logdir/test")
-        reinforce = ReinforceNetwork(env, alpha=1e-3)
-        num_episodes = 10
+        reinforce = ReinforceNetwork(env, alpha=1e-3, alpha_decay=.998, min_epsilon=0, gamma=.99)
+        num_episodes = 800
         for episode in range(num_episodes):
-            reinforce.training_episode(num_exploration_episodes=int(num_episodes * 2/3))
-            print("episode: {}, epsilon: {}, everage cumulative reward: {}".format(episode, reinforce.epsilon, reinforce.evaluate_average_cumulative_reward(100)))
+            reinforce.training_episode(debug=True)
 
         print('Average cumulative reward after episode:{} is: {}'.format(episode, reinforce.evaluate_average_cumulative_reward(100)))
 